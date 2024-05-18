ee6836827dc58516313b8e76f4e070f40c8bdc2f
MridulMohanta19 <mm8203@srmist.edu.in>
2024-05-18 19:25:44 +0530

    init

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 import express from "express";
 import bodyParser from "body-parser";
-import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
 import helmet from "helmet";
 import morgan from "morgan";
 import AWS from "aws-sdk";
 import domainRoute from "./routes/domain.js";
-
+const PORT = 5000;
 // config
 dotenv.config();
 const app = express();
@@ -17,7 +16,11 @@ app.use(helmet.crossOriginResourcePolicy({ policy : "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
+let origin = process.env.CORS_ORIGIN;
+app.use(cors({
+    credentials: true,
+    origin
+}));
 
 // aws
 AWS.config.update({
@@ -25,17 +28,14 @@ AWS.config.update({
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
     region: process.env.AWS_REGION
   });
-
-// routess
+console.log("PORT" , PORT);
+// routes
 app.use("/domain", domainRoute);
 
+app.get('/', function (req, res) {
+    res.send(`Hello World, running on port ${PORT}`);
+});
 
-// mongoose
-const PORT = process.env.PORT || 9000;
-mongoose.connect(process.env.MONGO_URL,{
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}).then(() => {
-    app.listen(PORT, () => console.log(`Server Port: ${PORT}`))
-}).catch((error) => console.log(`${error} did not connect`))
-
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
